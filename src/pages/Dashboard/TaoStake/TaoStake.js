b2bcfdeaa2e37235e4e7ea6b85d0cf3c47e12864
Zaid ali <65037315+ZaidAli60@users.noreply.github.com>
2023-08-19 23:43:45 +0500

    create a bittensor validators table

--- src/pages/Dashboard/TaoStake/TaoStake.js
+++ src/pages/Dashboard/TaoStake/TaoStake.js
@@ -1,7 +1,80 @@
+import { Table, Typography } from 'antd';
 import React from 'react'
 
+const { Title } = Typography;
+
 export default function TaoStake() {
+
+    const columns = [
+        {
+            title: 'Name',
+            dataIndex: 'name',
+        },
+        {
+            title: 'Chinese Score',
+            dataIndex: 'chinese',
+            sorter: {
+                compare: (a, b) => a.chinese - b.chinese,
+                multiple: 3,
+            },
+        },
+        {
+            title: 'Math Score',
+            dataIndex: 'math',
+            sorter: {
+                compare: (a, b) => a.math - b.math,
+                multiple: 2,
+            },
+        },
+        {
+            title: 'English Score',
+            dataIndex: 'english',
+            sorter: {
+                compare: (a, b) => a.english - b.english,
+                multiple: 1,
+            },
+        },
+    ];
+    const data = [
+        {
+            key: '1',
+            name: 'John Brown',
+            chinese: 98,
+            math: 60,
+            english: 70,
+        },
+        {
+            key: '2',
+            name: 'Jim Green',
+            chinese: 98,
+            math: 66,
+            english: 89,
+        },
+        {
+            key: '3',
+            name: 'Joe Black',
+            chinese: 98,
+            math: 90,
+            english: 70,
+        },
+        {
+            key: '4',
+            name: 'Jim Red',
+            chinese: 88,
+            math: 99,
+            english: 89,
+        },
+    ];
+    const onChange = (pagination, filters, sorter, extra) => {
+        console.log('params', pagination, filters, sorter, extra);
+    };
+
     return (
-        <div>TaoStake</div>
+        <div className='vh-100'>
+            <div className="card p-3">
+                <Title level={4} className='text-uppercase text-primary mb-3'>Bittensor Validators</Title>
+                <Table columns={columns} dataSource={data} onChange={onChange} />
+            </div>
+        </div>
     )
 }
