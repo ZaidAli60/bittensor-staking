6f1ff5160b232aa368e82648d3bddf7826d9f152
Zaid ali <65037315+ZaidAli60@users.noreply.github.com>
2023-08-19 22:09:13 +0500

    card design

--- src/pages/Dashboard/Home/Home.js
+++ src/pages/Dashboard/Home/Home.js
@@ -1,34 +1,59 @@
 import React from 'react'
+import { Col, Row, Typography } from 'antd'
+import { SiCoinmarketcap } from "react-icons/si"
+import { TbBrandGoogleAnalytics } from "react-icons/tb"
+
+const { Title } = Typography;
 
 export default function Home() {
     return (
-        <div>
-            <p>Home</p>
-            <p>Home</p>
-            <p>Home</p>
-            <p>Home</p>
-            <p>Home</p>
-            <p>Home</p>
-            <p>Home</p>
-            <p>Home</p>
-            <p>Home</p>
-            <p>Home</p>
-            <p>Home</p>
-            <p>Home</p>
-            <p>Home</p>
-            <p>Home</p>
-            <p>Home</p>
-            <p>Home</p>
-            <p>Home</p>
-            <p>Home</p>
-            <p>Home</p>
-            <p>Home</p>
-            <p>Home</p>
-            <p>Home</p>
-            <p>Home</p>
-            <p>Home</p>
-            <p>Home</p>
-            <p>Home</p>
+        <div className=''>
+            <Row gutter={16}>
+                <Col xs={24} lg={12}>
+                    <div className="card p-3 text-white mb-3" style={{ backgroundColor: "#fdedd4" }}>
+                        <div className='align-center justify-content-between mb-3'>
+                            <Title level={4} className='text-primary mb-0'>Maket Cap</Title>
+                            <div>
+                                <SiCoinmarketcap className='fs-5 text-primary' />
+                            </div>
+                        </div>
+                        <div className='align-center justify-content-between'>
+                            <Title level={5} className='mb-0 opacity-75'>Current price</Title>
+                            <Typography className='opacity-75'>$75.91</Typography>
+                        </div>
+                        <div className='align-center justify-content-between'>
+                            <Title level={5} className='mb-0 opacity-75'>Current price</Title>
+                            <Typography className='opacity-75'>$75.91</Typography>
+                        </div>
+                        <div className='align-center justify-content-between'>
+                            <Title level={5} className='mb-0 opacity-75'>Current price</Title>
+                            <Typography className='opacity-75'>$75.91</Typography>
+                        </div>
+                    </div>
+                </Col>
+                <Col xs={24} lg={12}>
+                    <div className="card p-3 text-white" style={{ backgroundColor: "#e1f9f1" }}>
+                        <div className='align-center justify-content-between mb-3'>
+                            <Title level={4} className='text-primary mb-0'>Network Status</Title>
+                            <div>
+                                <TbBrandGoogleAnalytics className='fs-5 text-primary' />
+                            </div>
+                        </div>
+                        <div className='align-center justify-content-between'>
+                            <Title level={5} className='mb-0 opacity-75'>Block number</Title>
+                            <Typography className='opacity-75'>$859.283</Typography>
+                        </div>
+                        <div className='align-center justify-content-between'>
+                            <Title level={5} className='mb-0 opacity-75'>Block number</Title>
+                            <Typography className='opacity-75'>$859.283</Typography>
+                        </div>
+                        <div className='align-center justify-content-between'>
+                            <Title level={5} className='mb-0 opacity-75'>Block number</Title>
+                            <Typography className='opacity-75'>$859.283</Typography>
+                        </div>
+                    </div>
+                </Col>
+            </Row>
         </div>
     )
 }
--- src/pages/Dashboard/SidebarItems.js
+++ src/pages/Dashboard/SidebarItems.js
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 import { IoAnalyticsOutline } from "react-icons/io5"
 import { MdHowToVote } from "react-icons/md"
-import { AppstoreOutlined, BarChartOutlined, CommentOutlined, TransactionOutlined, ToolOutlined, UserOutlined, SettingOutlined, InboxOutlined, PlusSquareOutlined, RiseOutlined, StarOutlined } from '@ant-design/icons';
+import { AppstoreOutlined } from '@ant-design/icons';
 
 const root = "/";
 
@@ -9,42 +9,4 @@ export const items = [
     { key: "home", icon: <AppstoreOutlined />, label: <Link to={root} className="text-decoration-none">Home</Link> },
     { key: "social", icon: <IoAnalyticsOutline />, label: <Link to={root + "tao-stake"} className="text-decoration-none">Stake Tao</Link> },
     { key: "vote", icon: <MdHowToVote />, label: <Link to={root + "vote"} className="text-decoration-none">Vote</Link> },
-    // {
-    //     key: "leads", icon: <RiseOutlined />, label: "Leads",
-    //     children: [
-    //         { key: "leads-0", icon: <RiseOutlined />, label: <Link to={root + "/leads"} className="text-decoration-none">Leads Home</Link> },
-    //         { key: "leads-1", icon: <RiseOutlined />, label: <Link to={root + "/leads/induction-orms"} className="text-decoration-none">Induction Forms</Link> },
-    //     ]
-    // },
-    // {
-    //     key: "transactions", icon: <TransactionOutlined />, label: "Transactions",
-    //     children: [
-    //         { key: "transactions-0", icon: <TransactionOutlined />, label: <Link to={root + "/transactions"} className="text-decoration-none">Main Transactions</Link> },
-    //         { key: "transactions-1", icon: <PlusSquareOutlined />, label: <Link to={root + "/transactions"} className="text-decoration-none">Create Transaction</Link> },
-    //         // { key: "transactions-2", icon: <FAIcon className="fa-solid fa-wand-magic-sparkles" />, label: <Link to={root + "/templates"} className="text-decoration-none">Templates/Checklists</Link> },
-    //         { key: "transactions-3", icon: <InboxOutlined />, label: <Link to={root + "/transactions/archives"} className="text-decoration-none">Archives</Link> },
-    //     ]
-    // },
-    // // { label: <Link to={root + "/transactions"} className="text-decoration-none">Transactions</Link>, key: "transactions", icon: <TransactionOutlined /> },
-    // // { label: <Link to={root + "/templates"} className="text-decoration-none">Templates</Link>, key: "templates", icon: <FAIcon className="fa-solid fa-wand-magic-sparkles" /> },
-    // { key: "collaborations", icon: <FaUserFriends />, label: <Link to={root + "/collaborations"} className="text-decoration-none">Collaborations</Link> },
-    // {
-    //     key: "assign", icon: <StarOutlined />, label: "Assign",
-    //     children: [
-    //         { key: "assign-0", icon: <StarOutlined />, label: <Link to={root + "/tasks-and-appointments"} className="text-decoration-none">Tasks & Appointments</Link> },
-    //     ]
-    // },
-    // { key: "compliance", icon: <CommentOutlined />, label: <Link to={root + "/compliance"} className="text-decoration-none">Compliance</Link> },
-    // { key: "growth", icon: <BarChartOutlined />, label: <Link to={root + "/growth"} className="text-decoration-none">Growth Dash</Link> },
-    // {
-    //     key: "tools", icon: <ToolOutlined />, label: "Tools",
-    //     children: [
-    //         { key: "tools-0", icon: <ToolOutlined />, label: <Link to={root + "/tools"} className="text-decoration-none">PDF Editor</Link> },
-    //         { key: "tools-1", icon: <ToolOutlined />, label: <Link to={root + "/tools"} className="text-decoration-none">PivottPresents</Link> },
-    //         { key: "tools-2", icon: <ToolOutlined />, label: <Link to={root + "/tools"} className="text-decoration-none">Calculators</Link> },
-    //     ]
-    // },
-    // // { label: <Link to={root + "/tools"} className="text-decoration-none">Tools</Link>, key: "tools", icon: <ToolOutlined /> },
-    // { key: "settings", icon: <SettingOutlined />, label: <Link to={root + "/settings"} className="text-decoration-none">Settings</Link> },
-    // { key: "refer-friend", icon: <UserOutlined />, label: <Link to={root + "/refer-friend"} className="text-decoration-none">Refer a Friend</Link> },
 ]
