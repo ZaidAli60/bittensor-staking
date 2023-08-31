87a75a5975b8306d7e289b38245bcba24e3cdd02
Zaid ali <65037315+ZaidAli60@users.noreply.github.com>
2023-09-01 01:26:51 +0500

    Tweaks

--- src/pages/Frontend/Balance/Balance.js
+++ src/pages/Frontend/Balance/Balance.js
@@ -1,7 +1,13 @@
+import { useThemeContext } from 'context/ThemeContext'
 import React from 'react'
 
 export default function Balance() {
+    const { theme } = useThemeContext()
     return (
-        <div>Balance</div>
+        <div className={`dashboard ${theme}`}>
+            <div className="container-fluid py-4">
+                <h5 className='mb-0'>Balance</h5>
+            </div>
+        </div>
     )
 }
