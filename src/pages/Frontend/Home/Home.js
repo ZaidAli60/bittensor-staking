8771029568bc073a30a36532383f0c30913846f1
Zaid ali <65037315+ZaidAli60@users.noreply.github.com>
2023-09-05 15:24:28 +0500

    Tweaks

--- src/pages/Frontend/Home/Home.js
+++ src/pages/Frontend/Home/Home.js
@@ -29,10 +29,12 @@ export default function Home() {
     const [amount, setAmount] = useState(0)
     const [totalBalance, setTotalBalance] = useState(null)
     const [rao, setRao] = useState(null)
-    const [stakeAmount, setStakeAmount] = useState(null)
+    const [stakeAmount, setStakeAmount] = useState(0)
     const [status, setStatus] = useState('');
     const [isFinalize, setIsFinalize] = useState(false)
     const [isFinalize1, setIsFinalize1] = useState(false)
+    const [filteredOptions, setFilteredOptions] = useState([]);
+    const [searchText, setSearchText] = useState('')
 
     const handleFatch = useCallback(async () => {
         setIsProcessing(true)
@@ -58,20 +60,29 @@ export default function Home() {
     }, [handleFatch])
 
     useEffect(() => {
+        // documents.sort((a, b) => a.name.localeCompare(b.name));
+        // Initialize filteredOptions with the same data as documents
+        const filtered = documents.filter((validator) =>
+            validator.name.toLowerCase().includes(searchText.toLowerCase())
+        );
+        setFilteredOptions(filtered);
+    }, [documents, searchText]);
+
+    useEffect(() => {
         setAccount(state.accounts.length > 0 ? state.accounts[0] : "")
     }, [state])
 
-    useEffect(() => {
-        setCurrentAPY(documents.length > 0 ? documents[0] : {})
-    }, [documents])
+    // useEffect(() => {
+    //     setValidator(documents.length > 0 ? documents[0] : {})
+    // }, [documents])
 
-    useEffect(() => {
-        setValidator(documents.length > 0 ? documents[0] : {})
-    }, [documents])
+    const handleSearch = (value) => {
+        setSearchText(value)
+    }
 
     const handleCurrentAPY = (value) => {
-        const currentapyvalue = documents.find(validator => validator.name === value);
-        setCurrentAPY(currentapyvalue);
+        const currentApyValue = documents.find((validator) => validator.name === value);
+        setCurrentAPY(currentApyValue);
     }
 
     const handleAccounts = (value) => {
@@ -89,10 +100,11 @@ export default function Home() {
         setRao(taoToRao)
     }, [amount])
 
-    const dataWithKeys = documents && documents?.map((record) => ({
-        ...record,
-        key: record.details?.hot_key.toString(), // Using the index as the key, but you should use a unique identifier
-    }));
+    const dataWithKeys = documents?.filter((record) => record.nominators !== 0)
+        .map((record) => ({
+            ...record,
+            key: record.details?.hot_key?.toString(), // Use optional chaining for safety
+        }));
 
     const columns = [
         {
@@ -124,7 +136,7 @@ export default function Home() {
             dataIndex: 'total_stake',
             sorter: (a, b) => a.total_stake - b.total_stake,
             render: (_, row) => {
-                return <Text className={`${theme === "dark" && "text-white"}`}>{row.total_stake?.toFixed(2)} TAO</Text>
+                return <Text className={`${theme === "dark" && "text-white"}`}> {parseInt(row?.total_stake).toLocaleString('en-US')} TAO</Text>
             }
         },
         {
@@ -345,11 +357,10 @@ export default function Home() {
                                             <Title level={5} className={`fontFamily ${theme === "dark" ? "text-white" : ""}`} style={{ fontSize: "13px" }}>Current Price</Title>
                                         </div>
                                         <div>
-
                                             <Title level={5} className={`fontFamily ${theme === "dark" ? "text-white" : ""}`}>$ {item?.price}</Title>
                                         </div>
                                         <div>
-                                            <Typography className={`fontFamily ${theme === "dark" ? "text-white" : ""}`}>24h Volume:  $ {Math.floor(parseFloat(volume_24h)).toLocaleString('de-DE')}</Typography>
+                                            <Typography className={`fontFamily ${theme === "dark" ? "text-white" : ""}`}>24h Volume: {parseFloat(volume_24h / 1e6).toFixed(3)}m</Typography>
                                         </div>
                                     </div>
                                 </Col>
@@ -420,15 +431,19 @@ export default function Home() {
                                     <Title level={4} className={`fontFamily ${theme === "dark" ? "text-uppercase text-white mb-3" : "text-uppercase text-primary mb-3"}`}>Bittensor Validators</Title>
                                     <Table columns={columns} bordered dataSource={dataWithKeys} loading={isProcessing} onChange={onChange} scroll={{ x: true }} className={`${theme === "dark" ? "dark-table" : "light-table"}`}
                                         expandable={{
-                                            expandedRowRender: (record) => (
-                                                <div className='px-5'>
-                                                    <Text className={`${theme === "dark" && "text-white"}`}>DESCRIPTION: {record.details?.description}</Text> <br />
-                                                    <Text className={`${theme === "dark" && "text-white"}`}>HOTKEY: {record.details?.hot_key}</Text> <br />
-                                                    <Text className={`${theme === "dark" && "text-white"}`}>WEBSITE: {record.details?.url}</Text>
-                                                </div>
-                                            ),
+                                            expandedRowRender: (record) => {
+                                                const absoluteUrl = record.details?.url.startsWith('http') ? record.details?.url : `https://${record.details?.url}`;
+                                                return (
+                                                    <div className='px-5' >
+                                                        <Text className={`${theme === "dark" && "text-white"}`}>Description: {record.details?.description}</Text> <br />
+                                                        <Text className={`${theme === "dark" && "text-white"}`}>Hotkey: {record.details?.hot_key}</Text> <br />
+                                                        <Text className={`${theme === "dark" && "text-white"}`}>Website: <a href={absoluteUrl} target="_blank" rel="noopener noreferrer">{record.details?.url}</a></Text>
+                                                    </div>
+                                                )
+                                            },
                                             rowExpandable: (record) => record.name !== 'Not Expandable',
                                         }}
+                                        pagination={{ pageSize: 15 }}
                                     />
                                 </div>
                             </Col>
@@ -438,21 +453,36 @@ export default function Home() {
                                         <Title level={4} className={`fontFamily ${theme === "dark" ? "text-uppercase text-white mb-3" : "text-uppercase text-primary mb-3"}`}>Staking Calculator</Title>
                                         <div className='card border-0 p-3 mb-3' style={{ backgroundColor: "#b5e61d" }}>
                                             <div className='d-flex justify-content-between'>
-                                                <Text className='fontFamily'>Current APY</Text>
-                                                <Text className='fontFamily'>{currentAPY.apy?.toFixed(2)}%</Text>
+                                                <Text className='fontFamily fw-bold'>Current APY</Text>
+                                                <Text className='fontFamily fw-bold'>{currentAPY.apy?.toFixed(2)}%</Text>
                                             </div>
                                         </div>
                                         <div style={{ width: "100%" }} className='mb-3'>
                                             <Space>
                                                 <Space.Compact >
-                                                    <Select value={currentAPY?.name} onChange={(value) => handleCurrentAPY(value)} style={{ width: "50%" }} className={`dashboard ${theme} ${theme === "dark" && "dark-dropdown"}`} >
-                                                        {documents && documents?.map((validator) => (
-                                                            <Option key={validator.name} value={validator.name}> {validator.name}</Option>
+                                                    <Select placeholder="Select a validator" style={{ width: "50%" }} className={`dashboard ${theme} ${theme === "dark" && "dark-dropdown"}`}
+                                                        showSearch
+                                                        value={currentAPY.name}
+                                                        onChange={handleCurrentAPY}
+                                                        onSearch={handleSearch}
+                                                        optionFilterProp="children"
+                                                        filterOption={false}
+                                                    >
+                                                        {filteredOptions.map((validator) => (
+                                                            <Option key={validator.name} value={validator.name}>
+                                                                {validator.name}
+                                                            </Option>
                                                         ))}
                                                     </Select>
                                                     <Input type='number' onChange={(e) => setTaoAmount(e.target.value)} placeholder='TAO Amount' className={`rtl-input ${theme === "dark" ? "bg-secondary text-white input-placeholder" : ""}`} />
                                                 </Space.Compact>
                                             </Space>
+                                        </div>
+                                        <div className={`p-3 mb-3 ${theme === "dark" ? "card bg-secondary border-1" : "card"}`}>
+                                            <div className="d-flex justify-content-between">
+                                                <Text className={`fontFamily ${theme === "dark" && "text-white"}`}>Commission</Text>
+                                                <Text className={`fontFamily ${theme === "dark" && "text-white"}`}>{currentAPY?.commission?.toFixed(2)}</Text>
+                                            </div>
                                         </div>
                                         <div className={`p-3 mb-3 ${theme === "dark" ? "card bg-secondary border-1" : "card"}`}>
                                             <div className="d-flex justify-content-between">
@@ -498,12 +528,18 @@ export default function Home() {
                                                     <Select
                                                         className={`fontFamily ${theme === "dark" && "dark-dropdown select-placeholder"}`}
                                                         style={{ width: "100%" }}
-                                                        placeholder="Select a validators"
+                                                        showSearch
                                                         value={validator.name}
                                                         onChange={(value) => handleValidators(value)}
+                                                        onSearch={handleSearch}
+                                                        placeholder="Select a validator"
+                                                        optionFilterProp="children"
+                                                        filterOption={false}
                                                     >
-                                                        {documents && documents?.map((validator) => (
-                                                            <Option key={validator.name} value={validator.name}>{validator.name}</Option>
+                                                        {filteredOptions.map((validator) => (
+                                                            <Option key={validator.name} value={validator.name}>
+                                                                {validator.name}
+                                                            </Option>
                                                         ))}
                                                     </Select>
                                                 </Form.Item>
@@ -515,17 +551,13 @@ export default function Home() {
                                                 </Form.Item>
                                             </Form>
                                         </div>
-                                        <div className={`p-3 mb-3  ${theme === "dark" ? "card bg-secondary border-1" : "card"}`}>
+                                        <div className='mb-3'>
                                             <div className="d-flex justify-content-between">
-                                                <Text className={`fontFamily ${theme === "dark" && "text-white"}`}>My Balance :</Text>
+                                                <Text className={`fontFamily ${theme === "dark" && "text-white"}`}>My Balance</Text>
                                                 <Text className={`fontFamily ${theme === "dark" && "text-white"}`}>{totalBalance} TAO</Text>
                                             </div>
                                         </div>
                                         <div className='mb-3'>
-                                            <div className="d-flex justify-content-between">
-                                                <Text className={`fontFamily ${theme === "dark" && "text-white"}`}>Available Amount</Text>
-                                                <Text className={`fontFamily ${theme === "dark" && "text-white"}`}>0 TAO</Text>
-                                            </div>
                                             <div className="d-flex justify-content-between">
                                                 <Text className={`fontFamily ${theme === "dark" && "text-white"}`}>Your Current Stake</Text>
                                                 <Text className={`fontFamily ${theme === "dark" && "text-white"}`}>{stakeAmount} TAO</Text>
@@ -542,6 +574,6 @@ export default function Home() {
                     </div>
                 </div>
             </div >
-        </div>
+        </div >
     )
 }
