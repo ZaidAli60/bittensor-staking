bf3e17b5b276bd95ba7576c9cd0f175296a7cf2e
Zaid ali <65037315+ZaidAli60@users.noreply.github.com>
2023-09-09 22:47:05 +0500

    font family add

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { ConfigProvider } from 'antd';
 import { useThemeContext } from './context/ThemeContext';
 
 function App() {
+
   const { theme } = useThemeContext()
 
   return (
@@ -11,14 +12,14 @@ function App() {
       theme={{
         token: {
           colorPrimary: '#323743',
+          fontFamily: ""
         },
       }}
     >
       <div className={`App dashboard ${theme} min-vh-100`}>
         <Routes />
       </div>
-    </ConfigProvider>
-
+    </ConfigProvider >
   );
 }
 
