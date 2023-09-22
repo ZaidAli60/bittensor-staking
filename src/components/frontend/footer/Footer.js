604612930d2f11dc76b1a38d0eb6a8e0f0277189
Zaid ali <65037315+ZaidAli60@users.noreply.github.com>
2023-09-23 01:29:29 +0500

    Tweaks

--- src/components/frontend/footer/Footer.js
+++ src/components/frontend/footer/Footer.js
@@ -4,7 +4,7 @@ import { useThemeContext } from 'context/ThemeContext'
 export default function Footer() {
     const { theme } = useThemeContext()
     return (
-        <footer className={`text-center footer dashboard ${theme}`}>
+        <footer className={`container-fluid text-center footer dashboard ${theme}`}>
             <div className='mb-5'>
                 <p className='fontFamily fw-bold mb-1' style={{ fontSize: "1rem" }} >Powered by <a href="https://firsttensor.com/" target="_blank" rel="noopener noreferrer"> <img src={`${theme === "dark" ? window.footerLogoLight : window.footerLogoDark}`} className='img-fluid' style={{ width: "130px", verticalAlign: "baseline" }} alt={window.appName} /></a> </p>
                 <p className='fontFamily mb-1'>To show support, please consider delegating TAO to FirstTensor.</p>
