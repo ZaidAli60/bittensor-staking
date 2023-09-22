7a8f41f5c8a89d4b1a4e989463e0bd27833d7ef3
Zaid ali <65037315+ZaidAli60@users.noreply.github.com>
2023-09-23 01:27:29 +0500

    add footer logo

--- src/components/frontend/footer/Footer.js
+++ src/components/frontend/footer/Footer.js
@@ -1,13 +1,14 @@
-import { useThemeContext } from 'context/ThemeContext'
 import React from 'react'
+import { useThemeContext } from 'context/ThemeContext'
 
 export default function Footer() {
     const { theme } = useThemeContext()
     return (
         <footer className={`text-center footer dashboard ${theme}`}>
             <div className='mb-5'>
-                <p className={`mb-1 fw-bold fontFamily ${theme === 'dark' && 'text-white'}`}>Copyright &copy; {window.year} <span><a className='text-decoration-none' href="https://bittensorstaking.com" target="_blank" rel="noopener noreferrer"> BittensorStaking.com </a></span></p>
-                <p className='fontFamily fw-bold'>Powered by <span><a className='text-decoration-none' href="https://firsttensor.com" target="_blank" rel="noopener noreferrer"> FirstTensor Validator </a></span></p>
+                <p className='fontFamily fw-bold mb-1' style={{ fontSize: "1rem" }} >Powered by <a href="https://firsttensor.com/" target="_blank" rel="noopener noreferrer"> <img src={`${theme === "dark" ? window.footerLogoLight : window.footerLogoDark}`} className='img-fluid' style={{ width: "130px", verticalAlign: "baseline" }} alt={window.appName} /></a> </p>
+                <p className='fontFamily mb-1'>To show support, please consider delegating TAO to FirstTensor.</p>
+                <p className={`mb-1 fw-bold fontFamily ${theme === 'dark' && 'text-white'}`} style={{ fontSize: "1rem" }}>Copyright &copy; {window.year} <span><a className='text-decoration-none' href="https://bittensorstaking.com" target="_blank" rel="noopener noreferrer"> BittensorStaking.com </a></span></p>
             </div>
         </footer>
     )
--- src/config/global.js
+++ src/config/global.js
@@ -2,12 +2,16 @@ import logoLight from "assets/images/logo2.png"
 import logoDark from "assets/images/logo.png"
 import image404light from "assets/images/404-light.svg"
 import image404dark from "assets/images/404-dark.svg"
+import footerLogoLight from "assets/images/firsttensor1.png"
+import footerLogoDark from "assets/images/firsttensor2.png"
 
 window.appName = process.env.REACT_APP_NAME
 window.logoLight = logoLight
 window.logoDark = logoDark
 window.image404Dark = image404dark
 window.image404Light = image404light
+window.footerLogoLight = footerLogoLight
+window.footerLogoDark = footerLogoDark
 
 // window.logoColor = logoColor
 // window.defaultDP = defaultDP
